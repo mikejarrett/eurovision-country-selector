3383448f36a653819cc4ac779bd9a4a9d61e78d7
Mike Jarrett <mike.d.jarrett@gmail.com>
2016-05-11 20:57:36 +0100

    Styling
    pep8 and pylint

--- eurovision/storage.py
+++ eurovision/storage.py
@@ -26,7 +26,7 @@ class ABCStorageMixin(metaclass=ABCMeta):
     @abstractmethod
     def retrieve_objects(self, **kwargs):
         raise NotImplementedError()
-        
+
     @abstractmethod
     def wipe(self):
         raise NotImplementedError()
@@ -35,22 +35,42 @@ class ABCStorageMixin(metaclass=ABCMeta):
 class PureMemoryStorage(ABCStorageMixin):
 
     storage_name = 'Pure Memory Storage'
-    
+
     def __init__(self):
         self._storage = []
 
     def persist(self, **kwargs):
+        """ Persist a single dictionary object to storage. """
         self._storage.append(kwargs)
-        
+
     def persist_multiple(self, objects):
+        """ Persist multiple dictionary objects to storage. """
         for obj in objects:
             self._storage.append(obj)
-        
+
     def retrieve_object(self, key, value):
+        """ Retrieve a single object.
+
+        Loop through objects in ``self._storage`` and look for a
+        ``key`` in the dict that matches the ``value`` passed in.
+
+        If there are multiple objects that match the ``key`` / ``value``
+        return the first one.
+
+        Args:
+            key (str): The key we use to obtain a value to check.
+            value (str): The value we compare the retrieved value against.
+
+        Returns:
+            dict: Representing and object.
+
+        Raises:
+             DoesNotExist: If not objects are found.
+        """
         for obj in self._storage:
             if obj.get(key) == value:
                 return obj
-        
+
         raise DoesNotExist(
             "Couldn't find {obj_storage} matching {key} {value}".format(
                 obj_storage=self.storage_name,
@@ -60,6 +80,13 @@ class PureMemoryStorage(ABCStorageMixin):
         )
 
     def retrieve_objects(self, **kwargs):
+        """ Retrieve all objects that match the passed in key-value pair.
+
+        If no "query" args are passed in return everything in ``self._storage``.
+
+        Returns:
+            list: Of dictionary objects.
+        """
         if not kwargs:
             return self._storage
 
@@ -70,11 +97,12 @@ class PureMemoryStorage(ABCStorageMixin):
                     objects.append(obj)
 
         return objects
-        
+
     def wipe(self):
+        """ Clear storage. Should only be used in testing. """
         self._storage = []
-        
-        
+
+
 class CSVStorage(PureMemoryStorage):
 
     storage_name = 'CSV Storage'
@@ -82,11 +110,12 @@ class CSVStorage(PureMemoryStorage):
     def __init__(self, file_name):
         super().__init__()
         self.file_name = file_name
-        
+
     def __enter__(self):
         return self
-        
+
     def __exit__(self, exc_type, exc_value, traceback):
+        """ Write objects in storage to ``self.file_name``. """
         if self._storage:
             with open(self.file_name, 'w') as file_object:
                 header = self._storage[0].keys()
@@ -94,6 +123,3 @@ class CSVStorage(PureMemoryStorage):
                 dict_writer.writeheader()
                 for obj in self._storage:
                     dict_writer.writerow(obj)
-        
-    def wipe(self):
-        self._storage = []
--- eurovision/tests/test_storage.py
+++ eurovision/tests/test_storage.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=protected-access,invalid-name
 import os
 
 from unittest2 import TestCase
@@ -11,7 +12,7 @@ class StorageTestInterface:
         self.assertEqual(len(self.storage._storage), 1)
         self.storage.persist(name='Eugene')
         self.assertEqual(len(self.storage._storage), 2)
-        
+
     def test_persist_multiple(self):
         self.assertEqual(len(self.storage._storage), 1)
         olivia = {'name': 'Olivia'}
@@ -25,24 +26,24 @@ class StorageTestInterface:
             ]
         )
         self.assertEqual(len(self.storage._storage), 4)
-        
+
     def test_retrieve_object(self):
         obj = self.storage.retrieve_object('name', 'Mike')
         self.assertDictEqual(obj, self.obj)
-        
+
     def test_retrieve_object_that_does_not_exist(self):
         with self.assertRaises(DoesNotExist):
             self.storage.retrieve_object('name', 'Steve')
-            
+
     def test_retrieve_objects(self):
         objects = self.storage.retrieve_objects(name='Mike')
         self.assertEqual(len(objects), 1)
         self.assertEqual(objects[0], self.obj)
-        
+
     def test_retrieve_objects_empty_list(self):
         objects = self.storage.retrieve_objects(bar='foo')
         self.assertEqual(objects, [])
-        
+
     def test_retrieve_objects_with_multiple_objects_returned(self):
         obj = {'name': 'Olivia'}
         self.storage.persist(**obj)
@@ -61,7 +62,7 @@ class TestPureMemoryStorage(StorageTestInterface, TestCase):
     def setUp(self):
         self.obj = {'name': 'Mike', 'foo': 'bar'}
         self.storage._storage.append(self.obj)
-        
+
     def tearDown(self):
         self.storage.wipe()
 
@@ -75,10 +76,10 @@ class TestCSVStorage(StorageTestInterface, TestCase):
     def setUp(self):
         self.obj = {'name': 'Mike', 'foo': 'bar'}
         self.storage._storage.append(self.obj)
-        
+
     def tearDown(self):
         self.storage.wipe()
-        
+
     def test_csvstorage__exit__(self):
         filename = 'created.csv'
         with CSVStorage(filename) as storage:
